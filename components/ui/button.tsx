7b7f8594d1e9f17cc9acd16411f56056e75a8b62
Mantasha Zubair <mantasha@wordscloud.in>
2025-02-26 15:45:00 +0530

    add css

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -37,17 +37,21 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  isLoading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, isLoading,children,...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        disabled={isLoading}
         {...props}
-      />
+      >
+      {isLoading ? 'Loading...' : children}
+      </Comp>
     );
   }
 );
