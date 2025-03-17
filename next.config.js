3a78a0bb4fdd783d9a6d44d8f1c557c0e7617359
zmantasha <mantasha@wordscloud.in>
2025-03-17 13:09:27 -0700

    remove strict mode

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false, // Disable Strict Mode
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -23,3 +24,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
