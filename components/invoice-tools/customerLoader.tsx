e3b0e24d873ee7e0a6677fa7e5599dfe2d88caee
zmantasha <mantasha@wordscloud.in>
2025-03-17 02:20:42 -0700

    add loader

--- components/invoice-tools/customerLoader.tsx
+++ components/invoice-tools/customerLoader.tsx
@@ -5,6 +5,7 @@ import CustomerTable from "./customerTable";
 import { useUser } from "@/hooks/UserContext";
 import axios from "axios";
 import Spinner from "../Spinner";
+import { CloudCog } from "lucide-react";
 
 export default function CustomerLoader(){
    const { user } = useUser();
@@ -12,25 +13,30 @@ export default function CustomerLoader(){
     const [customers, setCustomers] = useState<any[]>([]);
 
       const fetchCustomer=useCallback(async()=>{
+
         if (!user?.user?._id) return;
         try {
+          setIsLoading(true)
           const response = await axios.get(
-            `${process.env.NEXT_PUBLIC_SERVER}/api/v1/customer/${user.user._id}`
+            `${process.env.NEXT_PUBLIC_SERVER}/api/v1/customer/userId/${user.user._id}`
           );
+          
+          console.log(response)
           setCustomers(response.data || []);
+          
         }catch (error) {
           console.error("Failed to fetch invoices:", error);
-        } finally {
-          setIsLoading(false);
+        }finally{
+          setIsLoading(false)
         }
-      },[])
+      },[user?.user?._id, setCustomers])
 
       useEffect(() => {
         fetchCustomer();
        }, [fetchCustomer]);
 
       if (isLoading) {
-        return <Spinner loading={isLoading} color="teal" />;
+        return <Spinner loading={isLoading} color="gray" />;
       }
     
     return (
