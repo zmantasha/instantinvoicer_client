0ea9019442c56c91f8ebb430c88d7202109a70ab
zmantasha <mantasha@wordscloud.in>
2025-03-16 21:02:52 -0700

    add redirect path

--- app/user/customer/page.tsx
+++ app/user/customer/page.tsx
@@ -3,9 +3,13 @@
 import { Button } from "@/components/ui/button";
 import styles from "./customer.module.css"
 import CustomerLoader  from  "../../../components/invoice-tools/customerLoader" 
+import { useRouter } from "next/navigation";
 export default function Customer() {
 
-
+  const router= useRouter()
+  const handleNavigate=()=>{
+    router.push("/user/customer/add")
+  }
   return (
     <div className={styles.myCustomerPage}>
     <div className={styles.myCustomerContainer}>
@@ -13,7 +17,7 @@ export default function Customer() {
       {/* Header */}
       <div className={styles.customerHeader}>
         <h2 className={styles.customerTitle}>Customers</h2>
-        <Button className="bg-blue-600 hover:bg-[#0f7fe6] text-white px-4 py-2 rounded-md">Add New Customer</Button>
+        <Button className="bg-blue-600 hover:bg-[#0f7fe6] text-white px-4 py-2 rounded-md" onClick={handleNavigate}>Add New Customer</Button>
       </div>
 
       {/* Table */}
