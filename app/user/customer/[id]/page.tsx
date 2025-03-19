2d39e753558c9305f4eac1eb40f388b025ecaed2
zmantasha <mantasha@wordscloud.in>
2025-03-19 11:49:44 +0530

    add customerDetails into main and remove unnesssary imports

--- app/user/customer/[id]/page.tsx
+++ app/user/customer/[id]/page.tsx
@@ -1,22 +1,17 @@
 "use client"
-import CustomerList from "@/components/customer/CustomerList";
 import IndividualCustomerDetail from "@/components/customer/IndividualCustomerDetail";
 import styles from "./customerDetails.module.css";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { useUser } from "@/hooks/UserContext";
-import axios from "axios";
 import { useParams } from "next/navigation";
-import Spinner from "@/components/Spinner";
+
 
 export default function CustomerDetails() {
-    const { user } = useUser();
     const { id } = useParams(); // Get customer ID from URL
 
     // Fetch customer list ONLY on first load
 
 
     return (
-            <main>
+            <main className={styles.customerDetails}>
                 {id ? <IndividualCustomerDetail customerId={id as string} /> : <p>Select a customer to view details</p>}
             </main>    
     );
