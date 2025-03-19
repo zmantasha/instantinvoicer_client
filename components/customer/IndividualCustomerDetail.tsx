610afba9ffc96369ff3ef2c183bdc599e73c9aa8
zmantasha <mantasha@wordscloud.in>
2025-03-19 14:05:48 +0530

    add address details, otherDetails, contact details about customer

--- components/customer/IndividualCustomerDetail.tsx
+++ components/customer/IndividualCustomerDetail.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import Spinner from "../Spinner";
 import styles from "../../app/user/customer/[id]/customerDetails.module.css"; // Import CSS module
 import { Button } from "../ui/button";
-import { ChevronDown, Plus, Settings, X} from "lucide-react";
+import { ChevronDown, Edit, Plus, Settings, X} from "lucide-react";
 import { useRouter } from "next/navigation";
 
 interface Props {
@@ -15,7 +15,9 @@ export default function IndividualCustomerDetail({ customerId }: Props) {
   const [customer, setCustomer] = useState<any>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [activeTab, setActiveTab] = useState("overview");
-  const [addressToggle,setAddressToggle]= useState(false)
+  const [addressToggle,setAddressToggle]= useState(true)
+  const [otherToggle,setOtherToggle]= useState(true)
+  const [contactToggle,setContactToggle]= useState(true)
   const router= useRouter()
   // Fetch customer details when customerId changes
   useEffect(() => {
@@ -98,7 +100,7 @@ export default function IndividualCustomerDetail({ customerId }: Props) {
           
           <div className={styles.contentHeader}>
           <p>{customer.firstName}</p>
-          <Settings onClick={()=>handleNavigate("")}/>
+          <Settings className="text-gray-500 w-4 h-4" onClick={()=>handleNavigate("")}/>
           </div>
          
          <div className={styles.dropdowndown}>
@@ -107,22 +109,105 @@ export default function IndividualCustomerDetail({ customerId }: Props) {
          </div>
         {addressToggle && (
           <>
+          <div className={styles.addressHeader}>
+            {/* billing Address */}
           <div>
-           <p>Billing Address</p> 
+           <p>Billing Address:</p> 
          </div>
+          {/* edit icon */}
+          <Edit className="text-gray-500 w-4 h-4"/>
+         </div>
+         {/* billing details */}
+         <div className={styles.details}>
+          <p>{customer.billingAddress.street1}</p>
+          <p>{customer.billingAddress.street2}</p>
+          <p>{customer.billingAddress.city}</p>
+          <p>{customer.billingAddress.state}</p>
+          <p>{customer.billingAddress.pinCode}</p>
+          <p>{customer.billingAddress.country}</p>
+         </div>
+         <div className={styles.addressHeader}>
+          {/* shipping Address */}
           <div>
-           <p>Shipping Address</p> 
+           <p>Shipping Address:</p> 
+         </div>
+          {/* Edit icon  */}
+          <Edit className="text-gray-500 w-4 h-4"/>
+         </div>
+
+          {/* billing details */}
+          <div className={styles.details}>
+          <p>{customer.shippingAddress.street1}</p>
+          <p>{customer.shippingAddress.street2}</p>
+          <p>{customer.shippingAddress.city}</p>
+          <p>{customer.shippingAddress.state}</p>
+          <p>{customer.shippingAddress.pinCode}</p>
+          <p>{customer.shippingAddress.country}</p>
          </div>
          </>
         )}
+
+        {/* other details */}
          <div className={styles.dropdowndown}>
          <p>Other Details</p>
-          <ChevronDown className="text-[#0c69cc]" onClick={()=>handleNavigate("")}/>
+          <ChevronDown className="text-[#0c69cc]" onClick={()=>setOtherToggle(!otherToggle)}/>
          </div>
+         {otherToggle && (
+          <>
+           <div className={styles.otherDetails}>
+          <p><strong>Customer Type</strong> {customer.customerType}</p>
+          {customer.customerType==="business" && (
+            <>
+            <p><strong>Company Name</strong> {customer.companyName}</p>
+            <p><strong>TaxId</strong> {customer.taxId}</p>
+            {customer.creditLimit && <p><strong>Credit Limit</strong> {customer.creditLimit}</p>}
+
+            </>
+          )}
+          <p><strong>Default Currency</strong> {customer.currency}</p>
+          <p><strong>Work Phone</strong> {customer.workPhone}</p>
+          <p><strong>Mobile Phone</strong> {customer.mobilePhone}</p>
+          <p><strong>Status</strong> <span className={`px-2 py-1 text-sm rounded-full ${customer.status === "active" ? "bg-green-200 text-green-700" : "bg-red-200 text-red-700"}`}>{customer.status}</span></p>
+         </div>
+
+         </>
+         )}
+          {/* contact Info */}
          <div className={styles.dropdowndown}>
          <p>Contact Person</p>
-          <ChevronDown className="text-[#0c69cc]" onClick={()=>handleNavigate("")}/>
+          <ChevronDown className="text-[#0c69cc]" onClick={()=>setContactToggle(!contactToggle)}/>
          </div>
+         {contactToggle && (
+           <>
+           <div className={styles.otherDetails}>
+          {customer.customerType==="business" ? (
+            <>
+            {customer?.contacts?.map((contact:any,index:any)=>(
+              <>
+             <div className={styles.addressHeader}>
+             {/* billing Address */}
+              <div>
+                <p>Contacts:{index+1} </p> 
+              </div> 
+             </div>
+            {contact.name &&<p><strong>Name</strong> {contact.name}</p>}
+            {contact.email &&<p><strong>Email</strong> {contact.email}</p>}
+            {contact.workPhone && <p><strong>Work Phone</strong> {contact.workPhone}</p>}
+            {contact.mobilePhone && <p><strong>Mobile Phone</strong> {contact.mobilePhone}</p>}
+            {contact.designation && <p><strong>Designation</strong> {contact.designation}</p>}
+            </>
+            ))}
+      
+
+            </>): (
+            <>
+            <p className={styles.noData}>No contact persons found.</p>
+            </>
+          )}
+         </div>
+
+         </>
+         )}
 
 {/* 
           <div className={styles.details}>
