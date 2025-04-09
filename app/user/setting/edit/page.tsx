b90d24f3d8b1ccc0584fc610a300842062d7110a
mantasha zubair <mantasha@wordscloud.in>
2025-04-09 12:09:37 +0530

    remove console

--- app/user/setting/edit/page.tsx
+++ app/user/setting/edit/page.tsx
@@ -64,45 +64,7 @@ const formik = useFormik<FormValues>({
    console.log(error)
     }
   },
-});
-  // useEffect(()=>{
-  //   if (user?.user._id) {
-  //     generateInvoiceNumber();
-  //   }
-  // },[user])
-  // const generateInvoiceNumber = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       `http://localhost:8002/api/v1/invoice/invoices/userId/${user?.user._id}`
-  //     );
-  //     const invoices = response.data; // Assuming this is an array
-  //     console.log(invoices);
-  
-  //     let newInvoiceNumber = "INV-0001";  // Default if no previous invoice exists
-  
-  //     if (invoices && invoices.length > 0) {
-  //       const latestInvoice = invoices[invoices.length - 1]; // Get the last invoice
-  //       console.log(latestInvoice);
-  
-  //       if (latestInvoice.invoiceDetails?.number) {
-  //         // formik.setFieldValue("invoiceId", latestInvoice.invoiceDetails.number);
-  //         const lastNumber = parseInt(
-  //           latestInvoice.invoiceDetails.number.replace("INV-", ""), // Remove prefix
-  //           10
-  //         );
-  //         newInvoiceNumber = `INV-${String(lastNumber + 1).padStart(4, "0")}`;
-  //         formik.setFieldValue("invoiceId", latestInvoice.invoiceDetails.number);
-  //       }
-  //     }else{
-  //       formik.setFieldValue("invoiceId", newInvoiceNumber);
-  //     }
-  
-     
-  //   } catch (error) {
-  //     console.log("Error fetching invoices:", error);
-  //   }
-  // };
-  
+});  
 
    return (
     <>
